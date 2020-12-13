2c1a6522b49514a4daf6312bbb88a070fc46c776
Glitch (hello-webpage) <none>
2020-12-13 17:53:52 +0000

    🎋🏊🏼 Checkpoint ./serviceworker.js:577339/2836

--- serviceworker.js
+++ serviceworker.js
@@ -1,36 +1,14 @@
-// Licensed under a CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
-// http://creativecommons.org/publicdomain/zero/1.0/
+/* global workbox */
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"
+);
 
-// HTML files: try the network first, then the cache.
-// Other files: try the cache first, then the network.
-// Both: cache a fresh version if possible.
-// (beware: the cache will grow and grow; there's no cleanup)
+const { registerRoute } = workbox.routing;
+const { StaleWhileRevalidate, NetworkFirst } = workbox.strategies;
 
-const cacheName = 'files';
+registerRoute(
+  ({ request }) => request.headers.get("Accept").includes("text/html"),
+  new NetworkFirst()
+);
 
-addEventListener('fetch',  fetchEvent => {
-  const request = fetchEvent.request;
-  if (request.method !== 'GET') {
-    return;
-  }
-  fetchEvent.respondWith(async function() {
-    const fetchPromise = fetch(request);
-    fetchEvent.waitUntil(async function() {
-      const responseFromFetch = await fetchPromise;
-      const responseCopy = responseFromFetch.clone();
-      const myCache = await caches.open(cacheName);
-      return myCache.put(request, responseCopy);
-    }());
-    if (request.headers.get('Accept').includes('text/html')) {
-      try {
-        return await fetchPromise;
-      }
-      catch(error) {
-        return caches.match(request);
-      }
-    } else {
-      const responseFromCache = await caches.match(request);
-      return responseFromCache || fetchPromise;
-    }
-  }());
-});
+registerRoute(() => true, new StaleWhileRevalidate());
